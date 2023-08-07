eca96a87e7076071441481854827a0c0fe5e94a4
malthe991 <131148501+malthe991@users.noreply.github.com>
2023-08-07 09:25:50 +0200

    Add files via upload

---
+++ Basic opgaver 2/Basic 2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Threading;
+
+namespace Basic_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine(AbsoluteValute(6832));
+            Console.WriteLine(AbsoluteValute(-392));
+            Console.WriteLine(DivisibleBy2Or3(15, 30));
+            Console.WriteLine(DivisibleBy2Or3(2, 90));
+            Console.WriteLine(DivisibleBy2Or3(7, 12));
+            Console.WriteLine(IfConsistsOfUppercaseLetters("xyz"));
+            Console.WriteLine(IfConsistsOfUppercaseLetters("DOG"));
+            Console.WriteLine(IfConsistsOfUppercaseLetters("L9#"));
+            Console.WriteLine(IfGreaterThanThirdOne(new int[] { 2, 7, 12 }));
+            Console.WriteLine(IfGreaterThanThirdOne(new int[] { -5, -8, 50 }));
+            Console.WriteLine(IfNumberIsEven(721));
+            Console.WriteLine(IfNumberIsEven(1248));
+            //Absolute value
+            //Given an integer, write a method that returns its absolute value.
+            static int AbsoluteValute(int number1)
+            {
+                return Math.Abs(number1);
+            }
+
+
+            //Divisible by 2 or 3
+            //Given two integers, write a method that returns their multiplication if they are both divisible by 2 or 3, otherwise returns thier sum.
+            static int DivisibleBy2Or3(int number1, int number2)
+            {
+                if ((number1 % 2 == 0 || number1 % 3 == 0) && (number2 % 2 == 0 || number2 % 3 == 0))
+                {
+                    return number1 * number2;
+                }
+                else
+                {
+                    return number1 + number2;
+                }
+            }
+
+
+            //If consists of uppercase letters
+            //Given a 3 characters long string, write a method that checks if it consists only of uppercase letters.
+            static bool IfConsistsOfUppercaseLetters(string text)
+            {
+                if (text.Length == 3)
+                {
+                    foreach (char c in text)
+                    {
+                        if (!Char.IsUpper(c))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            //If greater than third one
+            //Given an array of 3 integers, write a method that checks if multiplication or sum of two first numbers is greater than third one.
+            static bool IfGreaterThanThirdOne(int[] array)
+            {
+                return array[0] + array[1] > array[2] || array[0] * array[1] > array[2];
+            }
+
+
+            //If number is even
+            //Given an integer, write a method that checks if it is even.
+            static bool IfNumberIsEven(int number1)
+            {
+                bool output = false;
+                if (number1 % 2 == 0)
+                {
+                    output = true;
+                }
+                return output; 
+            }
+            //If sorted ascending
+            //Given an array of three integers, write a method that checks if they are sorted in ascending order.
+            static bool IfSortedAscending(int[] array)
+            {                                                            
+
+            }
+
+        }
+    }
+}
---
+++ basic opgaver/basic/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+//hej
+namespace basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = AddAndMultiply(2, 4, 5);
+            Console.WriteLine(result);
+            Console.WriteLine(CelsiusToFahrenheit(0));
+            Console.WriteLine(CelsiusToFahrenheit(100));
+            Console.WriteLine(CelsiusToFahrenheit(-300));
+            Console.WriteLine(ElementaryOperations(3, 8)[0]);
+            Console.WriteLine(ElementaryOperations(3, 8)[1]);
+            Console.WriteLine(ElementaryOperations(3, 8)[2]);
+            Console.WriteLine(ElementaryOperations(3, 8)[3]);
+            Console.WriteLine(IsResultTheSame(2 + 2, 2 * 2));
+            Console.WriteLine(IsResultTheSame(9 / 3, 16 - 1));
+            Console.WriteLine(ModuloOperations(8, 5, 2));
+            Console.WriteLine(TheCubeOf(2));
+            Console.WriteLine(TheCubeOf(-5.5m));
+            Console.WriteLine(SwapTwoNumbers(87, 45));
+
+
+
+
+        }
+        //Add two numbers
+        //Given three numbers, write a method that adds two first ones and multiplies them by a third one.
+        static int AddAndMultiply(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int output =(firstNumber + secondNumber) * thirdNumber;
+
+            return output;
+        }
+        //Celsius to Fahrenheit
+        //Given a temperature in Celsius degrees, write a method that converts it to Fahrenheit degrees.Remember that temperature below -271.15°C (absolute zero) does not exist!
+        static string CelsiusToFahrenheit( float celsius)
+        {
+            if (celsius < -271.15)
+            {
+                return "Temperature below absolute zero!";
+            }
+            else
+            {
+                float fahrenheit = ((celsius * 9) / 5) + 32;
+                return fahrenheit.ToString();
+            }
+        }
+        //Elementary operations
+        //Given two integers, write a method that returns results of their elementary arithmetic operations: addition, substraction, multiplication, division.Remember that you can't divide any number by 0!
+        static decimal[] ElementaryOperations(decimal number1, decimal number2)
+        {
+            decimal[] result = new decimal[4];
+            result[0] = number1 + number2;
+            result[1] = number1 * number2;
+            result[2] = number1 - number2;
+            if (number1 != 0 && number2 != 0)
+            {
+                result[3] = number1 / number2;
+            }
+            else
+            {
+                result[3] = 0;
+            }
+            return result;
+        }
+        //Is result the same
+        //Given two different arithmetic operations(addition, subtraction, multiplication, division), write a method that checks if they return the same result.
+        static bool IsResultTheSame(int number1, int number2)
+        {
+            if (number1 == number2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Modulo operations
+        //Given three integers, write a method that returns first number divided modulo by second one and these divided modulo by third one.
+        static int ModuloOperations(int number1, int number2, int number3)
+        {
+            return(number1 % number2) % number3;
+        }
+       //The cube of
+       //Given a number, write a method that returns its cube.
+       static decimal TheCubeOf(decimal number1)
+        {
+            return number1 * number1 * number1;
+        }
+        //Swap two numbers
+        //Given two integers, write a method that swaps them using temporary variable.
+        static string SwapTwoNumbers(int number1, int number2)
+        {
+            string before = $"before number1 = {number1} , number2 = {number2}. ";
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+            string after = $"After number1 = {number1} , number2 = {number2} ";
+
+            return before + after ;
+        }
+
+    }
+}
