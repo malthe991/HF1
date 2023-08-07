ff3123084a124bd6e020cea1c4d80f37157c09b3
hyper991 <malthesimonsen@yahoo.dk>
2023-08-07 09:27:17 +0200

    Start

--- basic opgaver/basic/Program.cs
+++ basic opgaver/basic/Program.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.Design;
 using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
-//hej med dig
 namespace basic
 {
     internal class Program
