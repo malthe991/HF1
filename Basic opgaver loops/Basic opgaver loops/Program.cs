3a615375fa24f79a546ea976d4007694cb4a47fb
hyper991 <malthesimonsen@yahoo.dk>
2023-08-08 08:58:52 +0200

    Loops regnetabel

---
+++ Basic opgaver loops/Basic opgaver loops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Basic_opgaver_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
